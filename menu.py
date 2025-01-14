66f2e56342d26b83d8347e8b0ffb2cae452bf258
Aminshah10 <aminshahmohamadi1384@gmail.com>
2025-01-14 16:28:45 +0330

    getting username

--- menu.py
+++ menu.py
@@ -23,4 +23,26 @@ def load_from_file():
 def hasher(password):
     SALT = bcrypt.gensalt(8)
     hexpass = bcrypt.hashpw(password.encode(), SALT).hex()
-    return hexpass     
+    return hexpass
+
+user_names = load_from_file()
+
+user = input("username: ")
+
+if user in user_names:
+    console.print("user already exists. Please login.", style="error")
+    user = input("enter your username: ")
+    while user not in user_names:
+        console.print("username not found.", style="error")
+        user = input("enter your username: ")
+    
+    password = input("password: ")
+    while not bcrypt.checkpw(password.encode(), bytes.fromhex(user_names[user]["password"])):
+        console.print("Username or password is incorrect.", style="error")
+        password = input("password: ")
+
+    console.print("Login successful.", style="success")
+
+else:
+    password = input("password: ")
+    user_names[user] = {"user": user, "password": hasher(password)}    
